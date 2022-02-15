f8732c5d94e0b369d25c4e593683de3c64903d23
Ana Laura Sicz <85409130+AnaLauraS@users.noreply.github.com>
2022-02-14 23:52:14 -0300

    Corrección de animación
    Para que aparezca la información más linda y suavemente (ya no es display none-> in line)

--- js/aparicion.js
+++ js/aparicion.js
@@ -1,21 +1,25 @@
-function ocultar () {
-    var y = document.querySelectorAll('.oculto');
-    for (i=0; i<y.length; i++) {
-        y[i].setAttribute('style', 'display: none;');
-    };
-};
-
 function mostrar (a) {
     var x = document.getElementById(a);
-    x.setAttribute('style', 'display: inline;');
+   x.classList.replace('no-ver', 'ver');
+}
+
+function cambiar (a) {
+    var x = document.getElementById(a);
+   x.classList.replace('ver', 'no-ver');
+}
+function cambiarTodo () {
+    var y = document.querySelectorAll('.oculto');
+    for (i=0; i<y.length; i++) {
+       y[i].classList.replace('ver', 'no-ver');
+    };
 }
 
 function nueva(a) {
     var x = document.getElementById(a);
-    if (x.getAttribute('style')=="display: none;") {
-        ocultar();
-        mostrar(a);
-    } else {
-        ocultar();
-    }
+  if (x.classList.contains('ver')) {
+      cambiar(a);
+  }else{
+      cambiarTodo();
+      mostrar(a)
+  }
 };
