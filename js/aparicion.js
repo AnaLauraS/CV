3c6f52000f9379e0aba7425cecd6cd4c256a657e
Ana Laura Sicz <85409130+AnaLauraS@users.noreply.github.com>
2022-02-15 22:34:17 -0300

    Secciones "sobre mí" y "tecnologías"

--- js/aparicion.js
+++ js/aparicion.js
@@ -1,16 +1,25 @@
 function mostrar (a) {
     var x = document.getElementById(a);
    x.classList.replace('no-ver', 'ver');
+   var z = document.getElementsByClassName(a);
+    z[0].classList.add('mantener');
 }
 
 function cambiar (a) {
     var x = document.getElementById(a);
    x.classList.replace('ver', 'no-ver');
+   var z = document.getElementsByClassName(a);
+    z[0].classList.remove('mantener');
 }
+
 function cambiarTodo () {
     var y = document.querySelectorAll('.oculto');
     for (i=0; i<y.length; i++) {
        y[i].classList.replace('ver', 'no-ver');
+    };
+    var z = document.querySelectorAll('.titulos');
+    for (i=0; i<z.length; i++) {
+       z[i].classList.remove('mantener');
     };
 }
 
@@ -24,3 +33,18 @@ function nueva(a) {
   };
 };
 
+
+function paraPresentacion (){
+    var o = document.querySelector('.presentacion');
+    var p = document.querySelector('.btn-ocultar');
+    var t = document.querySelector('.btn-ver');
+    if (o.classList.contains('no-ver')){
+        o.classList.remove('no-ver');
+        p.setAttribute('style', 'display: inline;')
+        t.setAttribute('style', 'display: none;')
+    } else {
+        o.classList.add('no-ver');
+        p.setAttribute('style', 'display: none;')
+        t.setAttribute('style', 'display: inline;')
+    }    
+}
